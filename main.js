966cd138ce47d0d5acc25dac3f86f1e54e268761
Chris Antonie Pieterse <Chrisantoniep@gmail.com>
2019-01-06 17:20:05 +0200

    Added rendering

--- main.js
+++ main.js
@@ -63,6 +63,10 @@ function main() {
             modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
         },
     };
+
+    const buffers = initBuffers(gl);
+
+    drawScene(gl, programInfo, buffers);
 }
 
 /**
@@ -168,5 +172,102 @@ function initBuffers(gl) {
         position: positionBuffer,
     };
 }
+
+/**
+ * 
+ * @param {WebGLRenderingContext} gl WebGL rendering context. 
+ * @param {{ program: WebGLProgram, attribLocations: { vertexPosition: number}, uniformLocations: { projectionMatrix: WebGLUniformLocation, modelViewMatrix: WebGLUniformLocation }}} programInfo 
+ * @param {{position: WebGLBuffer}} buffers 
+ */
+function drawScene(gl, programInfo, buffers) {
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+    gl.clearDepth(1.0);                 // Clear everything
+    gl.enable(gl.DEPTH_TEST);           // Enable depth testing
+    gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+  
+    // Clear the canvas before we start drawing on it.
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  
+    /**
+     * Field of view is 45 degrees in radians.
+     * @type {Number} Degrees in radians.
+     */
+    const fieldOfView = 45 * Math.PI / 180;
+
+    /**
+     * Ratio that matches the display size of the canvas.
+     * @type {Number} width/height 
+     */
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    
+    /**
+     * Minimum visibility threshold.
+     * @type {Number}
+     */
+    const zNear = 0.1;
+
+    /**
+     * Maximum visibility threshold.
+     * @type {Number}
+     */
+    const zFar = 100.0;
+
+    /**
+     * Perspective matrix, a special matrix that is used to simulate the distortion of perspective in a camera.
+     * @type {mat4} 4x4 Matrix.
+     */
+    const projectionMatrix = mat4.create();
+  
+    // note: glmatrix.js always has the first argument as the destination to receive the result.
+    mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
+  
+    // Set the drawing position to the "identity" point, which is the center of the scene.
+    const modelViewMatrix = mat4.create();
+  
+    // Now move the drawing position a bit to where we want to
+    // start drawing the square.
+    mat4.translate(modelViewMatrix,     // destination matrix
+                   modelViewMatrix,     // matrix to translate
+                   [-0.0, 0.0, -6.0]);  // amount to translate
+  
+    // Tell WebGL how to pull out the positions from the position buffer into the vertexPosition attribute.
+    {
+      const numComponents = 2;  // pull out 2 values per iteration
+      const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+      const normalize = false;  // don't normalize
+      const stride = 0;         // how many bytes to get from one set of values to the next, 0 = use type and numComponents above
+      const offset = 0;         // how many bytes inside the buffer to start from
+      gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+      gl.vertexAttribPointer(
+          programInfo.attribLocations.vertexPosition,
+          numComponents,
+          type,
+          normalize,
+          stride,
+          offset);
+      gl.enableVertexAttribArray(
+          programInfo.attribLocations.vertexPosition);
+    }
+  
+    // Tell WebGL to use our program when drawing
+    gl.useProgram(programInfo.program);
+  
+    // Set the shader uniforms
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.projectionMatrix,
+        false,
+        projectionMatrix);
+        
+    gl.uniformMatrix4fv(
+        programInfo.uniformLocations.modelViewMatrix,
+        false,
+        modelViewMatrix);
+  
+    {
+      const offset = 0;
+      const vertexCount = 4;
+      gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+    }
+  }
 
 window.onload = main;
