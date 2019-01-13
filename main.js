9bfcc6ede751f06cd074e8d4be994ac779c4da94
Chris Antonie Pieterse <Chrisantoniep@gmail.com>
2019-01-13 17:55:04 +0200

    Added squar rotation

--- main.js
+++ main.js
@@ -73,7 +73,35 @@ function main() {
 
     const buffers = initBuffers(gl);
 
-    drawScene(gl, programInfo, buffers);
+    /**
+     * Time of last animation
+     * @type {Number}
+     */
+    let then = 0;
+
+    /**
+     * Rotation of the square
+     * @type {Number}
+     */
+    let squareRotation = 0.0;
+
+    /**
+     * Draw the scene repeatedly.
+     * @param {Number} now Time in milliseconds since the page loaded.
+     */
+    function render(now) {
+      now *= 0.001;  // convert to seconds
+
+      squareRotation += now - then;
+  
+      drawScene(gl, programInfo, buffers, squareRotation);
+
+      then = now;
+  
+      requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
 }
 
 /**
@@ -219,8 +247,9 @@ function initBuffers(gl) {
  * @param {WebGLRenderingContext} gl WebGL rendering context. 
  * @param {{ program: WebGLProgram, attribLocations: { vertexPosition: number, vertexColor: number }, uniformLocations: { projectionMatrix: WebGLUniformLocation, modelViewMatrix: WebGLUniformLocation }}} programInfo 
  * @param {{position: WebGLBuffer}} buffers 
+ * @param {Number} squareRotation Current rotation of the square. 
  */
-function drawScene(gl, programInfo, buffers) {
+function drawScene(gl, programInfo, buffers, squareRotation) {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
     gl.clearDepth(1.0);                 // Clear everything
     gl.enable(gl.DEPTH_TEST);           // Enable depth testing
@@ -270,6 +299,11 @@ function drawScene(gl, programInfo, buffers) {
     mat4.translate(modelViewMatrix,     // destination matrix
         modelViewMatrix,     // matrix to translate
         [-0.0, 0.0, -6.0]);  // amount to translate
+
+    mat4.rotate(modelViewMatrix,  // destination matrix
+        modelViewMatrix,  // matrix to rotate
+        squareRotation,   // amount to rotate in radians
+        [0, 0, 1]);       // axis to rotate around
 
     // Tell WebGL how to pull out the positions from the position buffer into the vertexPosition attribute.
     {
