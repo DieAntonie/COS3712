d488a56652b272214e9e9ce8046f10d7643a84d3
Chris Antonie Pieterse <Chrisantoniep@gmail.com>
2019-01-13 17:29:08 +0200

    Re-inserted position buffer population

--- main.js
+++ main.js
@@ -273,6 +273,25 @@ function drawScene(gl, programInfo, buffers) {
 
     // Tell WebGL how to pull out the positions from the position buffer into the vertexPosition attribute.
     {
+        const numComponents = 2;  // pull out 2 values per iteration
+        const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+        const normalize = false;  // don't normalize
+        const stride = 0;         // how many bytes to get from one set of values to the next, 0 = use type and numComponents above
+        const offset = 0;         // how many bytes inside the buffer to start from
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexPosition,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        gl.enableVertexAttribArray(
+            programInfo.attribLocations.vertexPosition);
+    }
+
+    // Tell WebGL how to pull out the colors from the color buffer into the vertexColor attribute.
+    {
         const numComponents = 4;  // pull out 2 values per iteration
         const type = gl.FLOAT;    // the data in the buffer is 32bit floats
         const normalize = false;  // don't normalize
